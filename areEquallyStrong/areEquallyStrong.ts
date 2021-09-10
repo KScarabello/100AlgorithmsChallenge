aa89644709801494249ff7674683fc7b29124667
KScarabello <kimberly.scarabello@gmail.com>
2021-09-09 18:06:41 -0700

    9-9-2021 more completed algos

--- areEquallyStrong/areEquallyStrong.ts
+++ areEquallyStrong/areEquallyStrong.ts
@@ -1,5 +1,11 @@
 export function areEquallyStrong(yourLeft: number, yourRight: number, friendsLeft: number, friendsRight: number): boolean {
-
+    let you = [yourLeft, yourRight];
+    let friend = [friendsLeft, friendsRight];
+    if(you.sort()[0] === friend.sort()[0] && you.sort()[1] === friend.sort()[1]){
+        return true;
+    } else {
+        return false;
+    }
 }
 
 // console.log(areEquallyStrong(10, 15, 15, 10))
