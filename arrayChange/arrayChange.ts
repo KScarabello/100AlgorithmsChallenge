5a762db4df1f6cd6f903b5d78d6ba8448cedd10a
KScarabello <kimberly.scarabello@gmail.com>
2021-09-16 22:40:15 -0700

    9-16 more algos complete

--- arrayChange/arrayChange.ts
+++ arrayChange/arrayChange.ts
@@ -1,5 +1,15 @@
 export function arrayChange(inputArray: number[]): number {
-
+    let total = 0;
+    for(let i = 0; i <= inputArray.length - 1; i++){
+        if(!inputArray[i + 1]){
+            total += inputArray[inputArray.length - 1] - inputArray[i] + 1;
+        }
+        if(inputArray[i] <= inputArray[i + 1]){
+            total += inputArray[i + 1] - inputArray[i] + 1
+        }
+    }
+    return total
 }
 
-// console.log(arrayChange([1, 1, 1]));
+
+console.log(arrayChange([1, 1, 1]));
--- arrayMaxConsecutiveSum/arrayMaxConsecutiveSum.ts
+++ arrayMaxConsecutiveSum/arrayMaxConsecutiveSum.ts
@@ -1,5 +1,13 @@
 export function arrayMaxConsecutiveSum(inputArray: number[], k: number): number {
-
+    let highest = 0;
+    for(let i = 0; i < inputArray.length; i++){
+        let subArrayTotal = inputArray.slice(i,  i + k);
+        if(subArrayTotal.reduce((a, b) => a + b) > highest){
+            highest = subArrayTotal.reduce((a, b) => a + b);
+        }
+    }
+    return highest;
 }
 
-// console.log(arrayMaxConsecutiveSum([2, 3, 5, 1, 6], 2));
+console.log(arrayMaxConsecutiveSum([2, 3, 5, 1, 6], 2));
+console.log(arrayMaxConsecutiveSum([1, 1, 1, 1, 100], 3));
